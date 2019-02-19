f753cfcf41367e69126f4a8d8ab70b7a2660a209
Joseph Surin <joseph.surin@gmail.com>
2019-02-19 14:44:54 +1100

    publish

--- src/util/index.js
+++
@@ -1,6 +0,0 @@
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Decemember']
-
-export function formatDate(ddmmyyyy) {
-	var [ day, month, year ] = ddmmyyyy.split('/')
-	return `${months[parseInt(month - 1)]} ${day}, ${year}`
-}
