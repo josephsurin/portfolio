41f3dfa89beee92d3e05749375ddbc43e1366610
Joseph Surin <joseph.surin@gmail.com>
2019-02-19 15:37:49 +1100

    update url

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,7 +8,7 @@ const Home = () => {
 				<div className="title">Joseph Surin</div>
 				<div className="subtitle">Student & Developer</div>
 				<div className="link-buttons">
-					<a title="Github" href="https://github.com/fumetsuu" className="fa-brands link-button github-button">github</a>
+					<a title="Github" href="https://github.com/josephsurin" className="fa-brands link-button github-button">github</a>
 					<a title="LinkedIn" href="https://www.linkedin.com/in/joseph-surin-0a756b17b/" className="fa-brands link-button linkedin-button">linkedin</a>
 					<a title="Email" href="mailto:joseph.surin@gmail.com" className="fa-solid link-button at-button">at</a>
 					<a title="Resume (PDF)" href="#" className="fa-solid link-button resume-button">file-alt</a>
