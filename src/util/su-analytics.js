1bbf95129aacefc4839347db74c62b2b2103c0f0
Joseph Surin <joseph.surin@gmail.com>
2019-03-01 14:15:20 +1100

    change analytics url

--- src/util/su-analytics.js
+++ src/util/su-analytics.js
@@ -1,5 +1,5 @@
 (function suAnalyticsInitialise() {
-	const API_URL = 'http://su-analytics.herokuapp.com/api'
+	const API_URL = 'https://su-analytics.herokuapp.com/api'
 	window.onhashchange = () => {
 		var url = window.location.href
 		if(url[url.length - 1] == '/') url = url.slice(0, url.length - 1)
