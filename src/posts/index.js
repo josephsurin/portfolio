7dbce2bc132f04f7ea17fde335bfdf85186c9956
Joseph Surin <joseph.surin@gmail.com>
2019-07-01 16:19:30 +1000

    remove post hyperlinks

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,50 +1,50 @@
 module.exports = [
-    {
-        title: 'ISITDTU 2019 Quals CTF Writeups',
-        slug: 'isitdtu-2019-quals-ctf-writeups',
-        date: '01/07/2019',
-        tags: 'ctf,infosec,writeup,crypto'
-    },
-    {
-        title: 'First Semester at Unimelb',
-        slug: 'first-sem-at-unimelb',
-        date: '27/06/2019',
-        tags: 'uni'
-    },
-    {
-        title: 'Hosting a Discord bot on Heroku',
-        slug: 'hosting-a-discord-bot-on-heroku',
-        date: '19/03/2019',
-        tags: 'development,project'
-    },
-    {
-        title: 'Experimenting with GraphQL',
-        slug: 'experimenting-with-graphql',
-        date: '02/03/2019',
-        tags: 'development,project,backend'
-    },
-    {
-        title: 'Unimelb programming proficiency test',
-        slug: 'unimelb-programming-proficiency-test',
-        date: '27/02/2019',
-        tags: 'uni'
-    },
-    {
-        title: 'How this blog works',
-        slug: 'how-this-blog-works',
-        date: '25/02/2019',
-        tags: 'development'
-    },
-    {
-        title: 'Github Pages and serving md',
-        slug: 'github-pages-and-serving-md',
-        date: '24/02/2019',
-        tags: 'development'
-    },
-    {
-        title: 'Generating hexagons with SVG',
-        slug: 'generating-hexagons-with-svg',
-        date: '22/02/2019',
-        tags: 'development,react,project,frontend'
-    }
+	{
+		title: 'ISITDTU 2019 Quals CTF Writeups',
+		slug: 'isitdtu-2019-quals-ctf-writeups',
+		date: '01/07/2019',
+		tags: 'ctf,infosec,writeup,crypto'
+	},
+	{
+		title: 'First Semester at Unimelb',
+		slug: 'first-sem-at-unimelb',
+		date: '27/06/2019',
+		tags: 'uni'
+	},
+	{
+		title: 'Hosting a Discord bot on Heroku',
+		slug: 'hosting-a-discord-bot-on-heroku',
+		date: '19/03/2019',
+		tags: 'development,project'
+	},
+	{
+		title: 'Experimenting with GraphQL',
+		slug: 'experimenting-with-graphql',
+		date: '02/03/2019',
+		tags: 'development,project,backend'
+	},
+	{
+		title: 'Unimelb programming proficiency test',
+		slug: 'unimelb-programming-proficiency-test',
+		date: '27/02/2019',
+		tags: 'uni'
+	},
+	{
+		title: 'How this blog works',
+		slug: 'how-this-blog-works',
+		date: '25/02/2019',
+		tags: 'development'
+	},
+	{
+		title: 'Github Pages and serving md',
+		slug: 'github-pages-and-serving-md',
+		date: '24/02/2019',
+		tags: 'development'
+	},
+	{
+		title: 'Generating hexagons with SVG',
+		slug: 'generating-hexagons-with-svg',
+		date: '22/02/2019',
+		tags: 'development,react,project,frontend'
+	}
 ]
