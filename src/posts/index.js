5c4efb84ebe9f593f47ccad8c43773520d943335
fumetsuu <joseph.surin@gmail.com>
2019-12-16 11:16:15 +1100

    fix watevr writeup

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,5 +1,11 @@
 module.exports = [
 	{
+		title: 'watevrCTF 2019 Writeups',
+		slug: 'watevrctf-2019-writeups',
+		date: '16/12/2019',
+		tags: 'ctf,infosec,writeup,crypto,web'
+	},
+	{
 		title: 'HackCon 2019 Writeups',
 		slug: 'hackcon-2019-writeups',
 		date: '24/08/2019',
